9c30ff00a0aaa7794fba9b4443bd98df3b50ce80
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2015-01-13 08:49:49 +0100

    changed depricated function naming

--- overrides.rb
+++ overrides.rb
@@ -4,7 +4,7 @@
  metapackage('rock.simulation').weak_dependencies = true
 
 if (Autoproj.user_config('pure_mars')) then
-    Autoproj.manifest.each_package do |p|
+    Autoproj.manifest.each_autobuild_package do |p|
         if p.kind_of?(Autobuild::Orogen) or p.name.include?("vizkit")
             Autoproj.manifest.add_exclusion p.name,"No-orogen-stuff"
         end
