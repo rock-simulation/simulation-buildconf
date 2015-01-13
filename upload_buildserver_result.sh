40d9794294b1df07415d9a89b46abba578aac435
Matthias Goldhoorn <matthias.goldhoorn@dfki.de>
2015-01-13 10:07:44 +0100

    Updated script to make paths relative

--- upload_buildserver_result.sh
+++ upload_buildserver_result.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync --delete -re "ssh -p2230" /data/chroots/14.04_amd64/data/jenkins/workspace/workspace/Mars-Homepage-Generation/Flavor/master/node/Ubuntu_LTS/api/ web@auv.informatik.uni-bremen.de:/home/web/www/mars/
+rsync --delete -re "ssh -p2230" $1 web@auv.informatik.uni-bremen.de:/home/web/www/mars/
