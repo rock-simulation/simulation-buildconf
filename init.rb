9cdbe435582e56e28487e642b9ec5eb90a9952a4
Anna Born <anna.born@dfki.de>
2022-01-28 09:27:41 +0100

    add dfkigit git config

--- init.rb
+++ init.rb
@@ -35,6 +35,7 @@ end
 require "autoproj/gitorious"
 Autoproj.gitorious_server_configuration('GITORIOUS', 'gitorious.org')
 Autoproj.gitorious_server_configuration('SPACEGIT', 'git.hb.dfki.de',:fallback_to_http => false)
+Autoproj.gitorious_server_configuration('DFKIGIT', 'git.hb.dfki.de',:fallback_to_http => false)
 Autoproj.gitorious_server_configuration('GITHUB', 'github.com', :http_url => 'https://github.com')
 
 Autoproj.env_inherit 'CMAKE_PREFIX_PATH'
